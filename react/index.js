0d9013266a9fb1644450f62b3df939f1333ac6d1
Alexander Chumovitsky <alexander.chumovitsky@identigy.com>
2018-10-29 13:06:02 +0300

    ios detected was fixed

--- react/index.js
+++ react/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 import { render} from 'react-dom';
 import App from './js/App';
 
+function iOS() {
+
+  var iDevices = [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod'
+  ];
+
+  if (!!navigator.platform) {
+    while (iDevices.length) {
+      if (navigator.platform === iDevices.pop()){ return true; }
+    }
+  }
+
+  return false;
+}
+
 function startApp() {
   //https://harrymoreno.com/2015/07/14/Deploying-a-React-App-to-Cordova.html
-  if(window.device && device.platform === 'iOS') {
+  if(iOS()) {
     styles.base.paddingTop = '20px'
   }
 
   render(<App />, document.getElementById("root"));
 }
+
 if (window.cordova) {
   document.addEventListener('deviceready', startApp, false);
 } else {
