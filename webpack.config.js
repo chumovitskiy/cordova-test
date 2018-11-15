b91762a834d8f0eae32b5aef2232b0402a1abf82
Alexander Chumovitsky <alexander.chumovitsky@identigy.com>
2018-11-15 19:29:09 +0300

    react files were moved

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    template: path.join(__dirname, "react/index.html"),
+    template: path.join(__dirname, "src/index.html"),
     filename: "./index.html"
 });
 module.exports = {
-    entry: path.join(__dirname, "react/index.js"),
+    entry: path.join(__dirname, "src/index.js"),
 	output: {
         path: path.join(__dirname, "www"),
         filename: "bundle.js"
